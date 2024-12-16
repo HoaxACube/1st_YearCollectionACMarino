d2392360743ec2d8c8d506695cd0c550bd7e4d4a
unknown <andrianmarino128@gmail.com>
2024-12-16 23:55:14 +0800

    account test2

--- ITCS_102_Project.py
+++ ITCS_102_Project.py
@@ -13,3 +13,11 @@ for x in range(1, 11):
     for y in range(1, x):
         print(x)
         print("account test")
+
+
+for x in range(1, 11):
+    for y in range(1, x):
+        print(x)
+        print("account test2")
+
+        
