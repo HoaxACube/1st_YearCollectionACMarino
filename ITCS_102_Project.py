bc0d7e71de19f7b100ca9e6669d35678c8cd80c9
unknown <andrianmarino128@gmail.com>
2024-12-17 00:38:30 +0800

    UI layout

--- ITCS_102_Project.py
+++ ITCS_102_Project.py
@@ -1,23 +1,44 @@
 #Project plan: Go through sa lahat ng activities and walk throughs with each and giving them information or ideas about it
 
 #plan 1. UI
+import os
+
+os.system('cls')
+
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+
+print("                                     ____")
+print("++                                      |")
+print("+                                       |")
+print("           Greetings!                    ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("|                                       +")
+print("|____                                  ++")
+
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+print("                                         ")
+
 #plan 2. functions of commands
+
 #plan 3. compilation of each activities
+
 #plan 4. error/bug testing
+
 #plan 5. video planning
-
-print("hello world")
-print("hello world")
-
-for x in range(1, 11):
-    for y in range(1, x):
-        print(x)
-        print("account test")
-
-
-for x in range(1, 11):
-    for y in range(1, x):
-        print(x)
-        print("account test2")
-
-        
